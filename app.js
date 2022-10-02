4bb0db04fa5e2b7702176107ff26a0c0da7dfa07
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2022-10-02 23:14:33 +0530

    get post added

--- app.js
+++ app.js
@@ -15,7 +15,30 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-	res.json('Hello World');
+	res.json('Welcome to express home page');
 });
+
+let products = [];
+
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.post('/api/products', (req, res) => {
+	let product = {};
+	product = req.body;
+	products.push(...product);
+	res.json(products);
+});
+
+app.put('/api/products/:id', (req, res)=> {
+	let product = {};
+	product = req.body;
+	products.map((prod, index) => {
+		if(prod.id == product.id) {
+			products[index] = { ...product }
+		}
+	})
+})
 
 app.listen(process.env.port || 8000, console.log('listening on port 8000'));
