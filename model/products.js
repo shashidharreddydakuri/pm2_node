f49a9005d22df4faa82b785d5186f6de2c7416e8
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2022-10-10 23:36:02 +0530

    product schema added

--- model/products.js
+++ model/products.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const mongoose = require('mongoose');
 
 const { connectMongoose } = require('../db-connect');
@@ -9,3 +10,16 @@ let productSchema = new mongoose.Schema({
 	name: String,
 	Price: Number,
 });
+
+let ProductModel = mongoose.model('products', productSchema);
+
+const getProductsMongoose = async () => {
+	connectMongoose();
+
+	let products = [];
+	query = {};
+
+	try {
+		products = await ProductModel.find(query, { _id: 0, _v: 0 });
+	} catch (err) {}
+};
